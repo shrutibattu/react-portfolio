49586840112d9ccf177b8fcd776e3b414dff6ab6
Shruti Battu <shrutibattu@MacBookPro-16inch.local>
2021-08-03 15:29:25 +0100

    made responsive

--- src/containers/Contact/Contact.jsx
+++ src/containers/Contact/Contact.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import styles from "./Contact.module.scss";
 
 const Contact = () => {
-  return (
-    <>
-      <p>Contact works</p>
-    </>
-  );
+  return <></>;
 };
 
 export default Contact;
